fc043107c6e06f1820c769ddd438c4285d157b01
Caideya <15585895191@163.com>
2023-09-15 19:29:23 +0800

    第一次更新

--- C_learn/初识C语言.cpp
+++ C_learn/初识C语言.cpp
@@ -263,15 +263,122 @@
 
 //指针
 
-int main()
-{
-    int a = 10;    //4个字节。
-    int* p = &a;   // &是取地址符。   p是一个指针变量，用来存放变量a这块内存的地址，它的类型是int*.
-    printf("%p\n", &a);   //%p是打印地址。
-    printf("%p\n", p);
-    *p = 20;             //*-解引用操作符，表示这一地址下的内容。 
-    printf("a=%d\n", a);
+//int main()
+//{
+//    int a = 10;    //4个字节。
+//    int* p = &a;   // &是取地址符。   p是一个指针变量，用来存放变量a这块内存的地址，它的类型是int*.
+//    printf("%p\n", &a);   //%p是打印地址。
+//    printf("%p\n", p);
+//    *p = 20;             //*-解引用操作符，表示这一地址下的内容。 
+//    printf("a=%d\n", a);
+//
+//    return 0;
+//}
 
+
+//分支和循环
+
+//C语言中，由一个分号隔开的就是一条语句。
+//int main()
+//{
+    //int age = 20;
+    //scanf("%d",&age);
+    //if (age < 18)                 //if或者else后面要执行多条语句，则要加大括号。
+    //    printf("未成年\n");
+    ////else(18 <= age < 28) 
+    //else if (age >= 18 && age < 28)
+    //    printf("青少年\n");
+    //else if (age >= 28 && age < 40)
+    //    printf("中年\n");
+    //else if (age >= 40 && age < 60)
+    //    printf("壮年\n");
+    //else if (age >= 60 && age <= 100)
+    //    printf("老年\n");
+    //else
+    //    printf("老寿星\n");
+
+    //C语言中 0表示假，非0表示真。
+
+
+    //int a = 0;
+    //int b = 2;
+    //if (a == 1)
+    //    if (b == 2)
+    //        printf("hehe\n");
+    //    else                         //else和距离最近的if进行匹配。而不是跟谁对齐就跟谁匹配。
+    //        printf("hehe\n");
+
+
+    //int a = 0;
+    //int b = 2;
+    //if (a == 1)
+    //{
+    //    if (b == 2)
+    //        printf("hehe\n");
+    //    else                         //else和距离最近的if进行匹配。而不是跟谁对齐就跟谁匹配。
+    //        printf("hehe\n");
+    //}                                //故If和else后面尽量加大括号。以防逻辑错误。
+   // return 0;
+//}
+
+
+//判断一个数是否是奇数。
+//
+//int main()
+//{
+//    int num = 0;
+//    scanf("%d", &num);
+//    if (num % 2 == 1)
+//    {
+//        printf("这个数是奇数\n");
+//    }
+//    else
+//    { 
+//        printf("这个数是偶数\n");
+//    }
+//    return 0;
+//}
+
+//输出1到100之间的奇数
+
+//int main()
+//{    
+//    int i = 1;
+//    while (i < 100)
+//    {
+//        if (i % 2 == 1)
+//            printf("%d  ", i);
+//        i++;
+//
+//    }
+//    printf("分割线\n");
+//    int num = 1;
+//    while (num < 100)
+//    {
+//        if (num % 2 == 1)
+//            printf("%d  ", num);
+//        num += 2;
+//       
+//    }
+//
+//  return 0;
+//}
+
+
+//switch多分支选择语句。
+
+int main()
+{/*
+    switch (整型表达式)
+    {
+        case 整型常量表达式；
+            语句；
+            break;    表示从这里跳出
+    }*/
+
+
+
+
+    //switch后面整型表达式的值决定从哪个分支进入，而break决定从哪个分支跳出来。
     return 0;
 }
-
